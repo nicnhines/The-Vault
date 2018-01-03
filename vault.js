103e91fe5836b49090bed464d0f110dabaf8e96e
nick <nicnines3@gmail.com>
2018-01-02 18:13:46 -1000

    finished

--- vault.js
+++ vault.js
@@ -1,4 +1,24 @@
 'use strict';
 module.exports = function() {
   
+let obj = {};
+
+let setValue =( key,value) => {
+    obj[key] = value;
+}
+
+let getValue = (key) => {
+    if ( key === undefined || !obj.hasOwnProperty(key) ) {
+        return null;
+    } else if ( obj.hasOwnProperty(key)) {
+        return obj[key];
+    }
+}
+
+
+return {
+    setValue: setValue,
+    getValue: getValue
+};
+
 };
